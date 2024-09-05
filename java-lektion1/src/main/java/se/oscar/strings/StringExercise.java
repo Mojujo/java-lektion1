6b722d85982aedac2468aae40bdfd95b9abac03b
Mojujo <oscar.tornqvist01@gmail.com>
2024-09-05 11:35:27 +0200

    Add files via upload

---
+++ java-lektion1/src/main/java/se/oscar/strings/StringExercise.java
@@ -0,0 +1,10 @@
+package se.oscar.strings;
+
+public class StringExercise {
+    public static void main(String[] args) {
+        String firstName = "Oscar";
+        String lastName = "Törnqvist";
+
+        System.out.print(firstName + " " + lastName);
+    }
+}
